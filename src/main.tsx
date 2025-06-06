b074e4d4628d16b0c21ad7790928ff9a1776d226
Mikhail Iakovlev <yakovmikemikle@gmail.com>
2025-06-06 22:40:16 +0300

    Add random dark color generation and set it as accent color on app initialization

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
+import { getRandomDarkColor } from './utils/colors'
 import './styles/main.css'
+
+// Set random accent color on app initialization
+document.documentElement.style.setProperty('--accent-color', getRandomDarkColor())
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
